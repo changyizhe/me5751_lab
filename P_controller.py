46d7775ff35e90f102197eeb6d4c5c8c9f7c76e6
Javilen <jdeas117@gmail.com>
2022-10-24 18:01:09 -0700

    All of our work from previous modules.

--- P_controller.py
+++ P_controller.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from calendar import c
+from sys import float_repr_style
 from E160_state import *
 from E160_robot import *
 import math
@@ -11,14 +13,18 @@ class P_controller:
 
 	def __init__(self, robot, logging = False):
 		self.robot = robot  # do not delete this line
-		self.kp = 0  # k_rho
-		self.ka = 0  # k_alpha
-		self.kb = 0  # k_beta
+		self.kp = 5  # k_rho
+		self.ka = 12  # k_alpha
+		self.kb = -1.5  # k_beta
+		self.finish = 1
+		self.fltrC_w = 50
+		self.fltrC_v = 100
 		self.logging = logging
 
 		if(logging == True):
-			self.robot.make_headers(['pos_X','posY','posZ','vix','viy','wi','vr','wr'])
-
+			#self.robot.make_headers(['pos_X','posY','posZ','vix','viy','wi','vr','wr'])
+			#self.robot.make_headers(['pos_X','rho', 'd_theta', 'alpha', 'beta'])
+			self.robot.make_headers(['pos_X','pos_y','vix','viy','wi','c_theta','goal_X','goal_y','d_theta'])
 		self.set_goal_points()
 
 	#Edit goal point list below, if you click a point using mouse, the points programmed
@@ -26,9 +32,11 @@ class P_controller:
 	def set_goal_points(self):
 		# here is the example of destination code
 		
-		self.robot.state_des.add_destination(x=100,y=20,theta=0)    #goal point 1
-		self.robot.state_des.add_destination(x=190,y=30,theta=1.57) #goal point 2
-
+		self.robot.state_des.add_destination(x=120,y=130,theta=-0.43)  #goal point 1
+		self.robot.state_des.add_destination(x=190,y=-0,theta=2.1)     #goal point 2
+		self.robot.state_des.add_destination(x=-150,y=-175,theta=0)    #goal point 3
+		self.robot.state_des.add_destination(x=-150,y=-25,theta=-1.57) #goal point 4
+		self.robot.state_des.add_destination(x=-25,y=200,theta=2.5)    #goal point 5
 
 
 	def track_point(self):
@@ -45,27 +53,63 @@ class P_controller:
 
 
 		# Most of your program should be here, compute rho, alpha and beta using d_pos and c_pos
-		# set new c_v = k_rho*rho, c_w = k_alpha*alpha + k_beta*beta
+		rho=math.sqrt((d_posX-c_posX)**2+(d_posY-c_posY)**2)
+		omega=math.atan2(d_posY-c_posY,d_posX-c_posX)
+		alpha=omega-c_theta
 		
-		c_v = 25 #randomly assigned c_v and c_w for demonstration purpose
-		c_w = 1.57
+        # Determine if forward or backwards method is to be used
+		if (- math.pi/2 < alpha <= math.pi/2):
+			beta = d_theta - omega # beta=omega-d_theta
+			c_v= self.kp*rho
+		else:
+			omega=math.atan2(d_posY-c_posY,c_posX-d_posX)
+			beta= -(omega + d_theta)  # beta= omega + d_theta
+			alpha =  c_theta + beta # - c_theta - beta
+			c_v= -self.kp*rho
+		
+        # Saturation filter for velocity
+		if c_v > self.fltrC_v: c_v = self.fltrC_v
+		if c_v < -self.fltrC_v: c_v = -self.fltrC_v
 
+		c_w=self.ka*alpha + self.kb*beta
+		# Saturation filter for angular velocity
+		if abs(c_w) > self.fltrC_w:
+			if abs(c_w) == self.fltrC_w:
+				c_w = self.fltrC_w
+			else:
+				c_w = -self.fltrC_w
+		
 		# self.robot.set_motor_control(linear velocity (cm), angular velocity (rad))
 		self.robot.set_motor_control(c_v, c_w)  # use this command to set robot's speed in local frame
 		
 		# you need to write code to find the wheel speed for your c_v, and c_w, the program won't calculate it for you.
-		self.robot.send_wheel_speed(phi_l = 6.0,phi_r = 6.0) #unit rad/s
+		# my fun attempt Deas
+		phi_l = (1/3)*c_v +4*c_w
+		phi_r = (1/3)*c_v -4*c_w
+		
+		if phi_l > 16: phi_l =16
+
+		if phi_l < -16: phi_l = -16
+
+		if phi_r > 16: phi_r = 16
+
+		if phi_r < -16: phi_r = -16
+
+		self.robot.send_wheel_speed(float("{:.1f}".format(phi_l)),float("{:.1f}".format(phi_r))) #unit rad/s phi_l = 6.0,phi_r = 6.0
 
 
 		# use the following to log the variables, use [] to bracket all variables you want to store
 		# stored values are in log folder
 		if self.logging == True:
-			self.robot.log_data([c_posX,c_posY,c_theta,c_vix,c_viy,c_wi,c_v,c_w])
+			#self.robot.log_data([c_posX,c_posY,c_theta,c_vix,c_viy,c_wi,c_v,c_w])
+			#self.robot.log_data([ c_posX , rho , d_theta, alpha, beta ])
+			self.robot.log_data([c_posX,c_posY,c_vix,c_viy,c_wi,c_theta,d_posX,d_posY,d_theta ])
 
-		if abs(c_posX - d_posX) < 90: #you need to modify the reach way point criteria
+		if abs(rho) < self.finish and abs(d_theta - c_theta) < 0.1: #you need to modify the reach way point criteria  if abs(c_posX - d_posX) < 80:
 			if(self.robot.state_des.reach_destination()): 
 				print("final goal reached")
 				self.robot.set_motor_control(.0, .0)  # stop the motor
+				self.robot.send_wheel_speed(.0, .0)
 				return True
 			else:
 				print("one goal point reached, continute to next goal point")
--- cost_map.py
+++ cost_map.py
@@ -1,3 +1,4 @@
+from importlib.util import set_loader
 import cv2
 import numpy as np
 import math
@@ -14,7 +15,7 @@ class cost_map:
 		self.map_width = int(self.graphics.environment.width*self.graphics.scale)
 		self.map_height = int(self.graphics.environment.height*self.graphics.scale)
 		try:
-			self.load_map(map = "maps/testmap.png") #load map
+			self.load_map(map = "maps/Group_testmap1.png") #load map  maps/testmap.png  maps/smol_test_map.png
 		except:
 			self.graphics.show_map_button.configure(state="disabled")
 			print ("no map loaded") #if fail to find the map png
@@ -22,7 +23,7 @@ class cost_map:
 		self.show_map()
 		self.compute_costmap()
 
-		# self.show_costmap()
+		#self.show_costmap()
 		self.save_vis_map(map = "maps/testcostmap2.png")
 
 	#load occupancy grid into self.map
@@ -67,10 +68,101 @@ class cost_map:
 		#It has nothing to do with brushfire, replace it
 		#A whilte pixel has value of 255, a black one is 0
 		#However, some block value may be dark gray
-		self.costmap[200:400][0:-1]=128
+		# self.costmap[200:400][0:-1]=128
+		#grid4=np.array([[0,1,0,0,0],[0,1,0,0,0],[0,1,0,0,0],[0,0,0,0,1],[0,0,0,0,1]],dtype=int)
+		grid=np.copy(self.costmap)
+		grid[grid == 0] = 1  #convert to island problem
+		grid[grid > 1] = 0
+		L=len(grid)
+		q=[]
+		q2=[]
+		gridImage=np.zeros((L,L),dtype=int)
+		brushMap=np.zeros((L,L),dtype=int)
+		for i in range(L):
+			for j in range(L):
+				if grid[i,j]==1:
+					q.append([i,j,0])
+					gridImage[i][j]
+				if grid[i,j]==0:
+					q2.append([i,j,0])
+		if len(q) == L*L  or len(q2) == L*L :
+			distance= -1
+		else:            
+			distance=0
+		'''
+        right=[0,1]
+        left=[0,-1]
+        up=[-1,0]
+        down=[1,0]
+        dir=[up, down, left, right]
+        '''
+		while len(q) > 0 and distance != -1:
+			[x,y,depth]=q.pop(0)
+			if grid[x][y] == 0:
+				distance = max(distance,depth)
+                # ensures that the max distance is used from the number of depth steps
+			for [i,j] in ([x+1,y],[x-1,y],[x,y+1],[x,y-1]): 
+                # around the point of interest
+				if i >=0 and i < L and j >=0 and j < L and gridImage[i][j] == 0:  
+                    # verifies the point to peak is both in the grid and open
+					gridImage[i][j] = 1
+                    # flags point on map as seen already
+					q.append([i,j,depth+1])
+					brushMap[i][j]=depth+1 
+				if i >=0 and i < L and j >=0 and j < L and gridImage[i][j] != 0 and grid[i][j] != 1: #see if we are too close to another 
+					if brushMap[i][j] > (depth+1):  # keep the smaller one for brushfire mission statement
+						brushMap[i][j] = (depth+1)
+                    # specifies that the point is one space away from the previous
+		# verifies that the brushfire map displays values limited from 2 to 255 ()
+		value=brushMap - 2000*grid # Ensuring the starting points are negative
+		value[value < 0] = 0  # Setting the walls to zero
+		
+		
+		value[value > 255] = 255 # normalizing values to a 255 max
+		bigNum= -1
+		for i in range(L):
+			for j in range (L):
+				bigNum = max(bigNum,value[i][j])
+		value= (255/bigNum)*value
+		for i in range(L):
+			for j in range (L):
+				value[i][j] = int(value[i][j])
+		
+		gravMap=np.copy(value)
+		for i in range(L): # Gravity map (1/r^2)
+			for j in range(L):
+				if gravMap[i][j] != 0:
+					gravMap[i][j] = gravMap[i][j]**(-2)
+				if gravMap[i][j] == 0:
+					gravMap[i][j] = 255
+		np.savetxt('Log/gravity_map1.csv',gravMap, delimiter=',')
+
+		gravMap2=np.copy(value)
+		for i in range(L): # Gravity map (1/r)
+			for j in range(L):
+				if gravMap2[i][j] != 0:
+					gravMap2[i][j] = gravMap2[i][j]**(-1)
+				if gravMap2[i][j] == 0:
+					gravMap2[i][j] = 255
+		np.savetxt('Log/gravity_map2.csv',gravMap2, delimiter=',')
+
+		gravMap3=np.copy(value)
+		for i in range(L): # Gravity map (abs(r-255))
+			for j in range(L):
+				if gravMap3[i][j] != 0:
+					gravMap3[i][j] = abs(gravMap3[i][j]-256)
+				if gravMap3[i][j] == 0:
+					gravMap3[i][j] = 255
+		np.savetxt('Log/gravity_map3.csv',gravMap3, delimiter=',')
+		
+		self.costmap=np.copy(gravMap3) # set our costmap (value) or our various gravity definitions as the output
+
+		np.savetxt('Log/cost_map.csv',self.costmap, delimiter=',')
 		pass
 
 	#scale costmap to 0 - 255 for visualization
 	def get_vis_map(self):
 		self.vis_map = np.uint8(255-self.costmap/4.0)
-		np.savetxt("Log/vismap.txt",self.vis_map)
+		#self.vis_map=np.uint(255-self.costmap/1.5)  # better view of map
+		np.savetxt("Log/vismap.csv",self.vis_map, delimiter=',')   # np.savetxt("Log/vismap.txt",self.vis_map)
+
